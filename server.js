a00a8420bf887da045424912338e40e62c419297
Joshie-Finster <77668881+Joshie-Finster@users.noreply.github.com>
2021-03-19 10:53:13 +0000

    UTC String

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+const { response } = require('express');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -38,6 +39,7 @@ app.get('/api/timestamp/:date_string',(req,res) =>{
 
   if (date_string.includes('-')){
     responseObject.unix = new Date(date_string).getTime()
+    responseObject.utc = new Date(date_string).toUTCString()
   }
   res.json(responseObject)
 })
