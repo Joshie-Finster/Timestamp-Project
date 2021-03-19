37b39e265307b8414995dcb8310c162fb81c0dcb
Joshie-Finster <77668881+Joshie-Finster@users.noreply.github.com>
2021-03-19 11:10:53 +0000

    passing fix

--- server.js
+++ server.js
@@ -32,18 +32,13 @@ var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
-app.get("/api/timestamp/", (req, res) => {
-  res.json({ unix: Date.now(), utc: Date() });
-});
 
 app.get("/api/timestamp/:date_string", (req, res) => {
   let dateString = req.params.date_string;
 
-  //A 4 digit number is a valid ISO-8601 for the beginning of that year
-  //5 digits or more must be a unix time, until we reach a year 10,000 problem
   if (/\d{5,}/.test(dateString)) {
-    const dateInt = parseInt(dateString);
-    //Date regards numbers as unix timestamps, strings are processed differently
+    const dateInt = new Date (parseInt(dateString));
+
     res.json({ unix: dateInt, utc: new Date(dateInt).toUTCString() });
   } else {
     let dateObject = new Date(dateString);
