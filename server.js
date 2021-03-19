d6e622e8aefe3889da529659118bddc0e5dacf95
Joshie-Finster <77668881+Joshie-Finster@users.noreply.github.com>
2021-03-19 13:22:04 +0000

    wont pass still

--- server.js
+++ server.js
@@ -39,9 +39,9 @@ app.get("/api/timestamp/:date_string", (req, res) => {
   let dateString = req.params.date_string;
 
   if (/\d{5,}/.test(dateString)) {
-    const dateInt = parseInt(new Date(date_string));
+    const dateInt = parseInt(dateString);
 
-    res.json({ unix: new Date(dateInt).getTime(), utc: new Date(dateInt).toUTCString() });
+    res.json({ unix: new Date(dateInt).valueOf(), utc: new Date(dateInt).toUTCString() });
   } else {
     let dateObject = new Date(dateString);
 
