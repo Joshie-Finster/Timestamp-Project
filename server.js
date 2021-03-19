1c1ecf18b5564d3185b7944ec6d11a89ba35ee5b
Joshie-Finster <77668881+Joshie-Finster@users.noreply.github.com>
2021-03-19 10:40:51 +0000

    first bit

--- server.js
+++ server.js
@@ -27,6 +27,11 @@ app.get("/api/hello", function (req, res) {
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+let responseObject ={};
+app.get('/api/timestamp/:date_string',(req,res) =>{
+  res.json(responseObject)
+})
