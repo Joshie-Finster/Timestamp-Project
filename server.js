a8567888d2f17b3fc4923768276dacf985b91d5e
Joshie-Finster <77668881+Joshie-Finster@users.noreply.github.com>
2021-03-22 11:20:17 +0000

    Added a Procfile

--- server.js
+++ server.js
@@ -2,57 +2,53 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-const { response } = require('express');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+const { response } = require("express");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
 
-app.get('/api/timestamp',(req,res)=>{
-  res.json({unix: Date.now(), utc: Date()})
-})
+app.get("/api/timestamp", (req, res) => {
+  res.json({ unix: Date.now(), utc: Date() });
+});
 app.get("/api/timestamp/:date_string", (req, res) => {
   let dateString = req.params.date_string;
 
   if (/\d{5,}/.test(dateString)) {
-    let date_string = parseInt(dateString)
+    let date_string = parseInt(dateString);
     const dateInt = new Date(date_string);
 
     res.json({ unix: dateInt.valueOf(), utc: new Date(dateInt).toUTCString() });
   } else {
     let dateObject = new Date(dateString);
-    console.log("dateObject = " + dateObject)
+    console.log("dateObject = " + dateObject);
 
     if (dateObject.toString() === "Invalid Date") {
       res.json({ error: "Invalid Date" });
     } else {
-      console.log("Date.Parse() = " + Date.parse(dateObject));
-      res.json({ unix: Date.parse(dateObject), utc: dateObject.toUTCString() });
-
+      console.log("dateObject valueOf = " + dateObject.valueOf());
+      res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
     }
   }
 });
